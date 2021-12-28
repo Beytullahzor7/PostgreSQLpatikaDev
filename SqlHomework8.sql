298536a1cfcc392df37e2de92bfb01533dc81f41
Beytullah ZOR <62347094+Beytullahzor7@users.noreply.github.com>
2021-12-29 01:50:35 +0300

    Homework8 Added

---
+++ SqlHomework8.sql
@@ -0,0 +1,20 @@
+-- Question 1
+/*CREATE TABLE employee (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	birthday DATE,
+	email VARCHAR(100)
+); */
+
+-- Question 2
+--Completed 50 data
+
+-- Question 3
+/*UPDATE employee SET name='Jack'
+WHERE name LIKE 'S%'; */
+
+
+-- Question 4
+DELETE FROM employee WHERE name ILIKE 'k%';
+
+
