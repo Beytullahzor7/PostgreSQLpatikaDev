2f3ec9d7ee1e92db0fc50ae09bbe897b238f571a
Beytullah ZOR <62347094+Beytullahzor7@users.noreply.github.com>
2021-12-30 17:50:40 +0300

    SqlHomework11

---
+++ SqlHomework11.sql
@@ -0,0 +1,29 @@
+-- Question 1
+/*
+(
+SELECT first_name FROM actor
+)
+UNION ALL
+(
+SELECT first_name FROM customer
+); */
+
+/*
+-- Question 2
+(
+SELECT first_name FROM actor
+)
+INTERSECT
+(
+SELECT first_name FROM customer
+); */
+-- Question 3
+(
+SELECT first_name FROM actor
+)
+EXCEPT
+(
+SELECT first_name FROM customer
+);
+
+
